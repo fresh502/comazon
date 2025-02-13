8782be8881addd034cf4f4f587719d651d2ac160
Sinrok Kim <fresh502@gmail.com>
2025-02-14 08:16:48 +0900

    add user api

---
+++ app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  // Destructuring assignment
+  const user = await prisma.user.findUnique({
+    where: { id },
+  });
+  res.send(user);
+});
+
+app.post("/users", async (req, res) => {
+  const user = await prisma.user.create({ data: req.body });
+  res.status(201).send(user);
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.update({
+    where: { id },
+    data: req.body,
+  });
+  res.send(user);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.user.delete({
+    where: { id },
+  });
+  res.send("Success delete");
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
