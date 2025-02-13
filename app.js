954fbc6f4f61909a0f4e8e8993a971bca2c1c25a
Sinrok Kim <fresh502@gmail.com>
2025-02-14 08:29:11 +0900

    add product api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
+// users
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
   res.send(users);
@@ -43,6 +44,44 @@ app.delete("/users/:id", async (req, res) => {
     where: { id },
   });
   res.send("Success delete");
+});
+
+// products
+app.get("/products", async (req, res) => {
+  const products = await prisma.product.findMany();
+  res.send(products);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+  res.send(product);
+});
+
+app.post("/products", async (req, res) => {
+  const product = await prisma.product.create({
+    data: req.body,
+  });
+  res.send(product);
+});
+
+app.patch("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.update({
+    where: { id },
+    data: req.body,
+  });
+  res.send(product);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.product.delete({
+    where: { id },
+  });
+  res.sendStatus(204);
 });
 
 app.listen(process.env.PORT || 3000, () =>
